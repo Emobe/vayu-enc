e6b6b813c568a6343604406ff33f897d177e9826
Emobe <anthony.r.shuker@gmail.com>
2015-12-28 22:37:39 +0000

    init

---
+++ server.js
@@ -0,0 +1,27 @@
+var mcrypt = require('mcrypt').MCrypt,
+    tripleDES = new mcrypt('tripeldes', 'ecb'),
+    fs = require('fs'),
+    input = '~/../build/api/uploads/',
+    output = '~/../build/data/';
+
+try{
+    var file = process.args[2],
+        user = process.args[3];
+}
+catch(err){
+    console.log('please specify a file');
+}
+
+tripleDES.open('cgThUA3LGnPQCFNSPEQkaAB9');
+
+
+fs.readFile(input + file, function(err, data){
+    var encrypted = tripleDES.encrypt(data);
+
+    fs.writeFile(output + user + '/' + file, function(err, data){
+        fs.unlink(input + file);
+    });
+});
+
+
+
